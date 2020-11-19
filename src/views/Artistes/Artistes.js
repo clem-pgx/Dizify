c2443d5ddb194a4ee74a1b024b47e90162f095bf
yeticlem <yeticlem79@hotmail.fr>
2020-11-19 11:18:38 +0100

    UPD artistes

--- src/views/Artistes/Artistes.js
+++ src/views/Artistes/Artistes.js
@@ -1,7 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { forwardRef } from 'react';
+import Avatar from 'react-avatar';
+import Grid from '@material-ui/core/Grid'
+// @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import ArtistesList from 'components/Artistes/ArtistesList';
-import withArtistLoading from 'components/Artistes/withArtistLoading';
+
+import MaterialTable from "material-table";
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import axios from 'axios'
+import Alert from '@material-ui/lab/Alert';
+import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from "components/CustomButtons/Button.js";
+import CardFooter from "components/Card/CardFooter.js";
+import { card } from "assets/jss/material-dashboard-react";
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Table from "components/Table/Table.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardBody from "components/Card/CardBody.js";
+
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
 
 const styles = {
   cardCategoryWhite: {
@@ -21,32 +72,196 @@ const styles = {
     textDecoration: "none"
   }
 };
-
 const useStyles = makeStyles(styles);
 
-function ArtistesApp() {
-  const classes = useStyles();
-  const ArtistesListLoading = withArtistLoading(ArtistesList);
-  const [appState, setAppState] = useState({
-    loading: false,
-    artistes: null,
-  });
+const api = axios.create({
+  baseURL: `http://localhost:8080`
+})
 
-  useEffect(() => {
-    setAppState({ loading: true });
-    const apiUrl = `http://localhost:8080/artistes`;
-    fetch(apiUrl)
-      .then((res) => res.json())
-      .then((artistes) => {
-        setAppState({ loading: false, artistes: artistes });
-      });
-  }, [setAppState]);
+function ArtistesApp() {
+
+  var columns = [
+    {title: "id", field: "id", hidden: true},
+    {title: "Avatar", render: rowData => <Avatar maxInitials={1} size={50} round={true} name={rowData === undefined ? " " : rowData.nom} />  },
+    {title: "Nom", field: "nom"},
+    {title: "Image", field: "image"}
+  ]
+  const [data, setData] = useState([]); //table data
+
+  //for error handling
+  const [iserror, setIserror] = useState(false)
+  const [errorMessages, setErrorMessages] = useState([])
+
+  useEffect(() => { 
+    api.get("/artistes")
+        .then(res => {
+            setData(res.data)
+         })
+         .catch(error=>{
+             console.log("Error")
+         })
+  }, [])
+
+  const handleRowUpdate = (newData, oldData, resolve) => {
+    //validation
+    let errorList = []
+    if(newData.nom === ""){
+      errorList.push("Stp ton nom")
+    }
+
+    if(errorList.length < 1){
+      api.patch("/artistes/"+newData.id, newData)
+      .then(res => {
+        const dataUpdate = [...data];
+        const index = oldData.tableData.id;
+        dataUpdate[index] = newData;
+        setData([...dataUpdate]);
+        resolve()
+        setIserror(false)
+        setErrorMessages([])
+      })
+      .catch(error => {
+        setErrorMessages(["Update failed! Server error"])
+        setIserror(true)
+        resolve()
+        
+      })
+    }else{
+      setErrorMessages(errorList)
+      setIserror(true)
+      resolve()
+
+    }
+    
+  }
+
+  const handleRowAdd = (newData, resolve) => {
+    //validation
+    let errorList = []
+    if(newData.nom === undefined){
+      errorList.push("stp ton nom")
+    }
+
+    if(errorList.length < 1){ //no error
+      api.post("/artistes", newData)
+      .then(res => {
+        let dataToAdd = [...data];
+        dataToAdd.push(newData);
+        setData(dataToAdd);
+        resolve()
+        setErrorMessages([])
+        setIserror(false)
+      })
+      .catch(error => {
+        setErrorMessages(["Cannot add data. Server error!"])
+        setIserror(true)
+        resolve()
+      })
+    }else{
+      setErrorMessages(errorList)
+      setIserror(true)
+      resolve()
+    }
+
+    
+  }
+
+  const handleRowDelete = (oldData, resolve) => {
+    
+    api.delete("/artistes/"+oldData.id)
+      .then(res => {
+        const dataDelete = [...data];
+        const index = oldData.tableData.id;
+        dataDelete.splice(index, 1);
+        setData([...dataDelete]);
+        resolve()
+        console.log(index);
+      })
+      .catch(error => {
+        setErrorMessages(["Delete failed! Server error"])
+        setIserror(true)
+        resolve()
+      })
+  }
+
+  const classes = useStyles();
   return (
-    <div className='App'>
-      <div className='artiste-container'>
-        <ArtistesListLoading isLoading={appState.loading} artistes={appState.artistes} />
-      </div>
+    <div className="App">
+      <GridContainer>
+            {/* <GridItem xs={12} sm={12} md={12}>
+              <Card>
+                <CardHeader color="primary">
+                  <h4 className={classes.cardTitleWhite}>Ajouter un Artiste</h4>
+                </CardHeader>
+                <CardBody>
+                  <GridContainer>
+                    <GridItem xs={12} sm={12} md={6}>
+                      <CustomInput
+                        labelText="Nom"
+                        id="nom"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                      />
+                    </GridItem>
+                    <GridItem xs={12} sm={12} md={6}>
+                      <CustomInput
+                        labelText="Image"
+                        id="image"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                      />
+                    </GridItem>
+                  </GridContainer>
+                </CardBody>
+                <CardFooter>
+                  <Button color="primary">Ajouter un artiste</Button>
+                </CardFooter>
+              </Card>
+            </GridItem> */}
+            <GridItem xs={12} sm={12} md={12}>
+            <Card>
+              <CardHeader color="primary">
+                <h4 className={classes.cardTitleWhite}>Artistes</h4>
+              </CardHeader>
+              <CardBody>
+                <div>
+                  {iserror && 
+                    <Alert severity="error">
+                        {errorMessages.map((msg, i) => {
+                            return <div key={i}>{msg}</div>
+                        })}
+                    </Alert>
+                  }       
+                </div>
+                <MaterialTable
+                  title=""
+                  columns={columns}
+                  data={data}
+                  icons={tableIcons}
+                  editable={{
+                    onRowUpdate: (newData, oldData) =>
+                      new Promise((resolve) => {
+                          handleRowUpdate(newData, oldData, resolve);
+                          
+                      }),
+                    onRowAdd: (newData) =>
+                      new Promise((resolve) => {
+                        handleRowAdd(newData, resolve)
+                      }),
+                    onRowDelete: (oldData) =>
+                      new Promise((resolve) => {
+                        handleRowDelete(oldData, resolve)
+                      }),
+                  }}
+                />
+              </CardBody>
+              </Card>
+            </GridItem>
+      </GridContainer>
     </div>
   );
 }
+
 export default ArtistesApp;
